6081e2e848db062e7eaa941687d438fb536eb488
onemanman <tringuyen14497@gmail.com>
2023-04-09 17:02:49 +0700

    swagger-configure

--- student-management/src/main/java/com/galaxy/studentmanagement/controller/StudentController.java
+++ student-management/src/main/java/com/galaxy/studentmanagement/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.galaxy.studentmanagement.controller;
 
 import com.galaxy.studentmanagement.model.Student;
 import com.galaxy.studentmanagement.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
+@Api(value="demo", description="Api description")
 public class StudentController {
 
     private final StudentService studentService;
@@ -18,12 +21,13 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
+    @ApiOperation(value = "Get all demo items", response = List.class)
     @GetMapping
     public List<Student> getAllStudents(){
         return studentService.getStudentList();
 
     }
+    @ApiOperation(value = "Get student by ID", response = Student.class)
     @GetMapping("/{stt}")
     public Student getStudentById(@PathVariable int stt){
          return studentService.getStudentById(stt);
