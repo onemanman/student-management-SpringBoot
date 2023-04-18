ee1609be8fa70baec6a7f146747dc7e3e56277ff
Tri Nguyen Van Cao <nvctri@tma.com.vn>
2023-04-18 08:34:03 +0700

    clean

--- student-management/src/main/java/com/galaxy/studentmanagement/controller/StudentController.java
+++ student-management/src/main/java/com/galaxy/studentmanagement/controller/StudentController.java
@@ -31,6 +31,7 @@ public class StudentController {
         return students;
     }
 
+
     @GetMapping(value = "/{stt}")
     public Student getStudentById(@PathVariable int stt){
         log.info("GET /student/{}",stt);
