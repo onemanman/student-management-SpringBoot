209046d88c8dd6246a8950935eb74766ff206487
Tri Nguyen Van Cao <nvctri@tma.com.vn>
2023-04-25 08:39:23 +0700

    add-web-client

--- student-management/src/main/java/com/galaxy/studentmanagement/StudentManagementApplication.java
+++ student-management/src/main/java/com/galaxy/studentmanagement/StudentManagementApplication.java
@@ -1,7 +1,7 @@
 package com.galaxy.studentmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
--- student-management/src/main/java/com/galaxy/studentmanagement/rest/UserRestClient.java
+++ student-management/src/main/java/com/galaxy/studentmanagement/rest/UserRestClient.java
@@ -1,40 +1,64 @@
 package com.galaxy.studentmanagement.rest;
 
 import com.galaxy.studentmanagement.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Repository
 public class UserRestClient {
+
+    @Autowired
+    private WebClient webClient;
+
     public List<User> getUserRestData(){
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<User>> response =
-                restTemplate.exchange
-                        (
-                          "http://localhost:8888/getData",
-                              HttpMethod.GET,
-                             null,
-                              new ParameterizedTypeReference<>(){}
-                        );
-        return response.getBody();
+        return webClient.get()
+                .uri("http://localhost:8888/getData")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<User>>() {})
+                .block();
     }
 
     public User createUserRestData(User newUser){
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange
-                        (
-                        "http://localhost:8888/getData",
-                            HttpMethod.POST,
-                            new HttpEntity<>(newUser),
-                            User.class
-                        )
-                        .getBody();
+        return webClient.post()
+                .uri("http://localhost:8888/getData")
+                .body(Mono.just(newUser), User.class)
+                .retrieve()
+                .bodyToMono(User.class)
+                .block();
     }
+
+//    public List<User> getUserRestData(){
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<List<User>> response =
+//                restTemplate.exchange
+//                        (
+//                          "http://localhost:8888/getData",
+//                              HttpMethod.GET,
+//                             null,
+//                              new ParameterizedTypeReference<>(){}
+//                        );
+//        return response.getBody();
+//    }
+
+//    public User createUserRestData(User newUser){
+//        RestTemplate restTemplate = new RestTemplate();
+//        return restTemplate.exchange
+//                        (
+//                        "http://localhost:8888/getData",
+//                            HttpMethod.POST,
+//                            new HttpEntity<>(newUser),
+//                            User.class
+//                        )
+//                        .getBody();
+//    }
 
 }
